fd9037776ece26fe4145b13c4cbb85b47d174d3a
Evgeniya Anufriyeva <2281474@gmail.com>
2023-09-07 22:09:27 +0300

    m2l1-commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,32 @@
-import { Card } from "./Card/Card"
+import Header from './Header/Header'
+// import Counter from './Counter/Counter'
+import Modal from './Modal/Modal'
+import { Component } from 'react'
+import TodoList from "./TodoList/TodoList"
 
+class App extends Component {
+  state = {
+    isShowModal: false
+  }
 
-const App = () => {
-  return (
-    <Card isOffline/>
-  )
+  showModal = () => {
+    this.setState({isShowModal: true})
+  }
+
+  closeModal = () => {
+    this.setState({isShowModal: false})
+  }
+
+  render() {
+    return (
+			<div className='container'>
+        <Header showModal={this.showModal} />
+				{/* <Counter /> */}
+				<TodoList/>
+				{this.state.isShowModal && <Modal closeModal={this.closeModal}>Some</Modal>}
+			</div>
+		)
+  }
 }
 
-export default App
+export default App;
